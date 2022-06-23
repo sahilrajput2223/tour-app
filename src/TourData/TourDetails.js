b81dcfbf3a3ee92c1b9632162ba993a29b39754f
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-23 22:52:25 +0530

    Tour Details Screen - Flex box design created

--- src/TourData/TourDetails.js
+++ src/TourData/TourDetails.js
@@ -3,16 +3,19 @@ import React from "react";
 import "./TourDetails.css"
 
 const TourDetails = ({ tourImg, tourTitle, tourDesc, tourPrice }) => {
+
     return (
-        <article>
+        <article className="artical">
             <img src={tourImg} alt={tourTitle} className="image" />
-            <footer>
-                <p>
+            <footer className="footer">
+                <div className="titleBar">
+                    <h4>{tourTitle}</h4>
+                    <h4 className="tourPrice">$ {tourPrice}</h4>
+                </div>
+                <div className="tourDesc">
                     {tourDesc}
-                </p>
-                <p>
-                    {tourPrice}
-                </p>
+                </div>
+                <button className="notInterestedBtn">not interested</button>
             </footer>
         </article>
     );
