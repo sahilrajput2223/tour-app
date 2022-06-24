cab20a62494b626de900cdd63bf69753de668db6
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-24 20:20:17 +0530

    Read More/Show less -- Button Design and React Logic Added

--- src/TourData/TourDetails.js
+++ src/TourData/TourDetails.js
@@ -4,6 +4,8 @@ import "./TourDetails.css"
 
 const TourDetails = ({ tourImg, tourTitle, tourDesc, tourPrice }) => {
 
+    const [isRead, setIsRead] = React.useState(true);
+
     return (
         <article className="artical">
             <img src={tourImg} alt={tourTitle} className="image" />
@@ -13,7 +15,12 @@ const TourDetails = ({ tourImg, tourTitle, tourDesc, tourPrice }) => {
                     <h4 className="tourPrice">$ {tourPrice}</h4>
                 </div>
                 <div className="tourDesc">
-                    {tourDesc}
+                    {isRead && tourDesc.substring(0, 200) + "..."}
+                    {!isRead && tourDesc}
+
+                    {isRead && <button className="readBtn" onClick={() => setIsRead(false)}>Read More</button>}
+                    {!isRead && <button className="readBtn" onClick={() => setIsRead(true)}>Show Less</button>}
+
                 </div>
                 <button className="notInterestedBtn">not interested</button>
             </footer>
